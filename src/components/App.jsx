79dea7b3052be3d82c884db9ed646cf39db0d465
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-07 16:58:28 -0500

    Implement VideoList component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,7 @@
+import exampleVideoData from '../../../src/data/exampleVideoData.js';
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
+
 var App = () => (
   <div>
     <nav className="navbar">
@@ -10,7 +14,8 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        {/* <div><h5><em>videoList</em> view goes here</h5></div> */}
+        <VideoList videos={exampleVideoData}/>
       </div>
     </div>
   </div>
