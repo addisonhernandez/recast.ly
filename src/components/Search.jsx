adefa24bf138017ab3617f7b6043c3ddf8f6e052
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-08 12:08:29 -0500

    Add onChange and onClick features to search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,20 @@
-var Search = () => (
-  <div className="search-bar form-inline">
-    <input className="form-control" type="text" />
-    <button className="btn hidden-sm-down">
-      <span className="glyphicon glyphicon-search"></span>
-    </button>
-  </div> 
-);
+var Search = (props) => {
+  let totallyNotState = '';
+
+  const handleChange = (event) => {
+    totallyNotState = event.target.value;
+    return totallyNotState;
+  };
+
+  return (
+    <div className="search-bar form-inline">
+      <input onChange={handleChange} className="form-control" type="text" />
+      <button onClick={() => props.searchSubmit(totallyNotState)} className="btn hidden-sm-down">
+        <span className="glyphicon glyphicon-search"></span>
+      </button>
+    </div>
+  );
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
