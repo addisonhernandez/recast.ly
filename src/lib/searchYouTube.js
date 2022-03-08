dee2eaa395bf7fcca6ac5d4f7b41f836b435a7e5
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-08 10:59:13 -0500

    Add ability to query a video server with ajax

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -5,7 +5,22 @@ $.ajaxPrefilter(function (settings, _, jqXHR) {
 });
 
 var searchYouTube = (query, callback) => {
-  // TODO
+  // Use jQuery to send a GET request to the search endpoint. This is the only time you should use jQuery in this sprint
+  //  Accept a callback function that is invoked with the videos array that is returned from hitting the endpoint
+  //  Accept an q string to search for
+  //  Make sure all the tests for searchYouTube are passing. You can open the tests with npm test
+
+  // https://www.youtube.com/results?search_query=  + 'search query'
+
+  $.ajax({
+    url: 'https://app-hrsei-api.herokuapp.com/api/recastly/videos',
+    type: 'GET',
+    data: ({ q: query }),
+    contentType: 'application/json',
+    success: callback,
+    error: (error) => console.error('Failed to fetch videos', error)
+  });
 };
 
 export default searchYouTube;
+
