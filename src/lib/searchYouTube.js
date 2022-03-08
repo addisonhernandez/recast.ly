76aa9c0a94840c0f2f48eb6a5bf41e20bf31cc94
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-08 16:04:54 -0500

    Refactor using fetch API
    Almost completely working. The first response comes back too slowly for
    the page to render, but subsequent queries work as expected.

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -12,15 +12,72 @@ var searchYouTube = (query, callback) => {
 
   // https://www.youtube.com/results?search_query=  + 'search query'
 
-  $.ajax({
-    url: 'https://app-hrsei-api.herokuapp.com/api/recastly/videos',
-    type: 'GET',
-    data: ({ q: query }),
-    contentType: 'application/json',
-    success: callback,
-    error: (error) => console.error('Failed to fetch videos', error)
-  });
+
+
+  // $.ajax({
+  //   url: 'https://app-hrsei-api.herokuapp.com/api/recastly/videos',
+  //   type: 'GET',
+  //   data: ({ q: query }),
+  //   contentType: 'application/json',
+  //   success: callback,
+  //   error: (error) => console.error('Failed to fetch videos', error)
+  // });
+
+  //========================= Experimental Features ===========================================
+
+  const searchURL = `https://app-hrsei-api.herokuapp.com/api/recastly/videos?q=${query}`;
+  const initSettings = {
+    method: 'GET',
+    headers: {
+      Authorization: API_KEY,
+      'Content-Type': 'application/json'
+    },
+  };
+
+  fetch(searchURL, initSettings)
+    .then(response => {
+      response.json().then(data => callback(data));
+    })
+    .catch(error => console.error('Fetch failed', error));
 };
 
 export default searchYouTube;
+
+/*
+fetch('URL' [, init])
+  .then(callback)
+  .catch(error => console.error('oops', error))
+
+URL: https://www.youtube.com/results?search_query=
+
+init: custom settings object
+{
+  method: 'GET'
+  headers: {
+    authorization: API_KEY
+  }
+
+}
+  // var getData = async function() {
+  //   try {
+  //     const res = await fetch(`https://app-hrsei-api.herokuapp.com/api/recastly/videos?q=${query}`);
+  //     if (!res.ok) {
+  //       const message = 'Danger Will Robinson';
+  //       throw new Error(message);
+  //     }
+  //     const data = await res.json();
+  //     const result = {
+  //       status: res.status + '-' + res.statusText,
+  //       headers: {
+  //         "content-type": res.headers.get('Content-Type'),
+  //         "Content-Length": res.headers.get('Content-Length'),
+  //       },
+  //       data: data,
+  //     }
+
+  //     console.log('result: ', result);
+  //   }
+  // }
+
+*/
 
